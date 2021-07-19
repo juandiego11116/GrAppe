84692422c955d848e0e2405ac2fb332f1d77d15c
juandiego11116 <71513912+juandiego11116@users.noreply.github.com>
2021-07-19 11:48:14 -0500

    Update
    Order code, delete variable ¨ok¨ and use equial

--- Anagrams/src/anagrams/Anagrams.java
+++ Anagrams/src/anagrams/Anagrams.java
@@ -19,46 +19,31 @@ public class Anagrams {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
-        Scanner read =  new Scanner (System.in);
+
+        Scanner read = new Scanner(System.in);
         System.out.println("Enter the first phrase");
         String first = read.nextLine();
         System.out.println("Enter the second phrase");
         String second = read.nextLine();
-       
+
         first = first.replace(" ", "");
-        char phrase1 []= first.toCharArray();
+        char phrase1[] = first.toCharArray();
         Arrays.sort(phrase1);
         second = second.replace(" ", "");
-        char phrase2 []= second.toCharArray();
+        char phrase2[] = second.toCharArray();
         Arrays.sort(phrase2);
-        
-        if(phrase1.length != phrase2.length){
+
+        if (phrase1.length != phrase2.length) {
             System.out.println("The Phrase are not anagrams");
             System.exit(0);
-            
-        } 
-        boolean ok = false;
-        for (int i=0; i<phrase1.length; i++){
-            if (phrase1[i] != phrase2[i]){
+        }
+        
+        if (Arrays.equals(phrase2, phrase1)) {
+                System.out.println("Congratuletions The Phrase are  anagrams");
+                
+            } else {
                 System.out.println("The Phrase are not anagrams");
-                System.exit(0);
-            }else{
-                ok = true;
             }
-            
-        }
-        if (ok == true){
-            System.out.println("Congratuletions The Phrase are  anagrams");
-        }
-        
-        /*if(phrase1[i] == phrase2[i] || i == phrase1.length || i == phrase2.length ){
-            System.out.println("Congratuletions The Phrase are  anagrams");
-        }else {
-            System.out.println("The Phrase are not anagrams");
-        }*/
-        
-        
     }
-    
+
 }
